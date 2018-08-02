9c969dddeafa1103a85eb1b954509a04a9023deb
zach <zaspringer@gmail.com>
2018-08-02 15:44:33 -0600

    Added some logging functionality

--- backend/scripts/checkLinkStatus.js
+++ backend/scripts/checkLinkStatus.js
@@ -1,12 +1,19 @@
 require('dotenv').config({ path: __dirname + '/../.env'})
 const Massive = require('massive');
 const axios = require('axios')
+const fs = require('fs')
+const errorFile = '../logging/deadLinks.txt'
 
 Massive(process.env.CONNECTION_STRING).then(db => {
     let completed = {}
     let changedProjectsActiveIds = []
     let changedProjectsBrokenIds = []
 
+    function writeErrorToFile(errorString) {
+        fs.appendFile(errorFile, errorString, function (err) {
+            if (err) throw err;
+          });
+    }
     function checkFinished() {
         let finished = true;
         for(let key in completed) {
@@ -15,11 +22,11 @@ Massive(process.env.CONNECTION_STRING).then(db => {
         if(finished) {
             if(brokenIds.length) {
                 db.projects.update({id: changedProjectsBrokenIds}, {active: false})
-                .catch(err => console.log('err'))
+                .catch(err => writeErrorToFile('\n Could not update broken'))
             }
             if(activeIds.length) {
               db.projects.update({id: changedProjectsActiveIds}, {active: true}) 
-              .catch(err => console.log('err'))
+              .catch(err => writeErrorToFile('\n Could not update active'))
             }
         }
     }
@@ -32,7 +39,6 @@ Massive(process.env.CONNECTION_STRING).then(db => {
                     completed[i] = true;
                     if(response.status === 200) {
                         if(!links[i].active) {
-                            links[i].active = true;
                             changedProjectsActiveIds.push(links[i].id)
                         }
                     }
@@ -40,7 +46,6 @@ Massive(process.env.CONNECTION_STRING).then(db => {
                 })
                 .catch( err => {
                     if(links[i].active) {
-                        links[i].active = false 
                         changedProjectsBrokenIds.push(links[i].id)
                     }  
                     checkFinished()
@@ -49,12 +54,11 @@ Massive(process.env.CONNECTION_STRING).then(db => {
             else {
                 completed[i] = true;
                 if(links[i].active) {
-                    links[i].active = false
                     changedProjectsBrokenIds.push(links[i].id)
                 } 
                 checkFinished()
             }
         }
     })
-    .catch(err => console.log('Error on db', err))
+    .catch(err => writeErrorToFile('\n Could not connect to database'))
 })
