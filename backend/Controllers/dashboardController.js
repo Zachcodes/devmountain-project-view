285f7f19fa9715c76c52da2d9857e060e4340940
zach <zaspringer@gmail.com>
2018-08-08 16:45:45 -0600

    Copied over code for admin dashboard to staff dashboard. Next step is to give admins ability to reset passwords

--- backend/Controllers/dashboardController.js
+++ backend/Controllers/dashboardController.js
@@ -42,9 +42,28 @@ module.exports = {
     loadStaffDashboard: (req, res) => {
         const db = req.app.get('db')
         let {userId} = req.session
-        //TODO: Concatenate groups together before serving them to the frontend - take logic written in above functions
-        db.get_staff_ratings({userId}).then(dbRes => {
-            res.status(200).send(response)
+        db.get_staff_ratings({userId}).then(ratedProjects => {
+            let splitRatedProjects = splitPersonalAndGroup(ratedProjects)
+            let personalRated = splitRatedProjects.personalProjects
+            let groupRated = splitRatedProjects.groupArr
+
+            db.get_unrated_projects({userId}).then(unratedProjects => {
+                let splitUnratedProjects = splitPersonalAndGroup(unratedProjects)
+                let personalUnrated = splitUnratedProjects.personalProjects
+                let groupUnrated = splitUnratedProjects.groupArr
+
+                let returnGroups = {
+                    rated: {
+                        personal: personalRated,
+                        group: groupRated
+                    },
+                    unrated: {
+                        personal: personalUnrated,
+                        group: groupUnrated
+                    }
+                }
+                res.status(200).send(returnGroups)
+            })
         })
     },
     loadStudentDashboard: (req, res) => {
--- frontend/src/Components/Staff.js
+++ frontend/src/Components/Staff.js
@@ -1,19 +1,104 @@
 import React, {Component} from 'react'
 import axios from 'axios'
+import {ToastContainer, toast} from 'react-toastify'
 
-export default class Staff extends Component {
+//components 
+import RatedProject from './RatedProject'
+import UnratedProject from './UnratedProject'
+
+export default class Admin extends Component {
     constructor() {
         super()
+        this.state = {
+            ratedPersonal: [],
+            ratedGroup: [],
+            unratedPersonal: [],
+            unratedGroup: []
+        }
     }
     componentDidMount() {
-        axios.get('/api/loadDashboard/admin').then(response => {
-            console.log(response)
+        axios.get('/api/loadDashboard/staff').then(response => {
+            this.updateProjects(response.data)
         })
     }
+
+    updateProjects = (projectsArr, toastMessage) => {
+        let {rated, unrated} = projectsArr
+        let {group: groupUnrated, personal: personalUnrated} = unrated; 
+        let {group: groupRated, personal: personalRated} = rated;
+        this.setState({
+            ratedPersonal: personalRated,
+            ratedGroup: groupRated,
+            unratedPersonal: personalUnrated,
+            unratedGroup: groupUnrated
+        }, () => {
+            toast.success(toastMessage)
+        })
+    }
+
+    updateRatingOnProject = (index, key, newRating) => {
+        let copiedArr = [...this.state[key]]
+        copiedArr[index].rating = newRating
+        let tempObj = {}
+        tempObj[key] = copiedArr
+        this.setState(tempObj, () => {
+            toast.success('Successfully updated project')
+        })
+    }
+
     render() {
+        let {ratedGroup, ratedPersonal, unratedPersonal, unratedGroup} = this.state
         return (
             <div>
-                Staff!
+               {
+                    ratedPersonal.map((project, index) => {
+                            return (
+                                <RatedProject 
+                                project={project} 
+                                key={project.projectId}
+                                updateProjects={this.updateProjects}
+                                index={index}
+                                updateRatingOnProject={this.updateRatingOnProject}
+                                type="personal"/>
+                            )
+                    })
+                }
+                {
+                    ratedGroup.map((project, index) => {
+                            return (
+                                <RatedProject 
+                                project={project} 
+                                key={project.projectId}
+                                updateProjects={this.updateProjects}
+                                index={index}
+                                updateRatingOnProject={this.updateRatingOnProject}
+                                type="group"/>
+                            )
+                    })
+                } 
+                {
+                    unratedPersonal.map((project, index) => {
+                            return (
+                                <UnratedProject 
+                                project={project} 
+                                key={project.projectId}
+                                updateProjects={this.updateProjects}
+                                type="personal"/>
+                            )
+                    })
+                } 
+                {
+                    unratedGroup.map((project, index) => {
+                            return (
+                                <UnratedProject 
+                                project={project} 
+                                key={project.projectId}
+                                updateProjects={this.updateProjects}
+                                type="group"/>
+                            )
+                    })
+                } 
+                <ToastContainer/>
             </div>
         )
     }
