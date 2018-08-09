397261f274063b2570b8ea11b8da5e6692b123e0
zach <zaspringer@gmail.com>
2018-08-09 12:05:03 -0600

    Haven't finished but committing now because I just spent two hours trying to write javascript when I could have just used sql

--- backend/scripts/averageRatingsNightly.js
+++ backend/scripts/averageRatingsNightly.js
@@ -7,11 +7,7 @@ Massive(process.env.CONNECTION_STRING).then(db => {
 
     //This will get all projects with no average rating so we can create and also filters out those that have no rating at all
     db.get_projects_with_no_average().then(projectIdArray => {
-        let projectIds = projectIdArray.map( project => project.id)
-        db.project_ratings.find({project_id: projectIds}).then( projectsWithNoAvgRating => {
-            let grouped = _.groupBy(projectsWithNoAvgRating, 'project_id')
-            console.log(grouped)
-        }).catch(err => console.log(22222))
+        console.log(projectIdArray)
     })
 })
 
--- backend/scripts/db/get_projects_with_no_average.sql
+++ backend/scripts/db/get_projects_with_no_average.sql
@@ -1,8 +1,9 @@
-select p.id
+select p.id, round(avg(pr.rating), 2)
 from average_project_ratings ar 
 right join projects p
 on p.id = ar.project_id
 full join project_ratings pr 
 on pr.project_id = p.id
 where ar.id is null
-and pr.id is not null;
+and pr.id is not null
+group by p.id;
