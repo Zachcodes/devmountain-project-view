5054e26f86b5f5b759e50ba00724d32f96a17360
zach <zaspringer@gmail.com>
2018-12-27 15:48:07 -0700

    Added the ability to add a new image and edit. Next step will be to allow saving and adding to the database

--- src/Components/StudentProject.js
+++ src/Components/StudentProject.js
@@ -7,45 +7,53 @@ export default class StudentProject extends Component {
             ...props.project,
             activeImageIndex: 0,
             edit: false,
-            newImage: ''
+            newImage: '',
+            addedImages: []
         }
     }
 
-    handleChange = (value, key, imageId) => {
+    handleChange = (value, key) => {
         let obj = {}
-        if(imageId) {
-            let updatedImages = this.state.images.map( image => {
-                if(image.project_image_id === imageId) {
-                    let copy = {...image}
-                    copy.image_url = value 
-                    return copy
-                }
-                else {
-                    return image
-                }
-            })
-            obj[key] = updatedImages
-        }
-        else {
-            obj[key] = value 
-        }
+        obj[key] = value 
+        this.setState(obj)
+    }
+
+    handleImageChange = (value, key, imageId) => {
+        let obj = {}
+        let updatedImages = this.state[key].map( image => {
+            if(image.project_image_id === imageId) {
+                let copy = {...image}
+                copy.image_url = value 
+                return copy
+            }
+            else {
+                return image
+            }
+        })
+        obj[key] = updatedImages
         this.setState(obj)
     }
 
     addNewImage = () => {
-        // TODO: Needs to go update the database and then when you get back do an update on the parent data
-        console.log(111111, ' getting clicked', this.state.newImage)
+       let tempId;
+       let copy = this.state.addedImages.slice()
+       copy.length ? tempId = copy[copy.length -1].project_image_id + 1 : tempId = 1;
+       copy.push({project_image_id: tempId, image_url: this.state.newImage})
+       this.setState({
+           addedImages: copy,
+           newImage: ''
+       })
     }
 
     render() {
-        let {description, images, project_link, project_name, walkthrough_link, activeImageIndex, members, type, edit, newImage} = this.state;
+        let {description, images, project_link, project_name, walkthrough_link, activeImageIndex, members, type, edit, newImage, addedImages} = this.state;
         // TODO: change this to be an actual placeholder image
         let activeImage = images.length ? images[activeImageIndex].image_url : 'https://s3-us-west-1.amazonaws.com/project-browser-development/pictures/148e1a41-b327-4652-8272-5d4f35f5b617_IMG_0359.jpg'
         let restOfImages = images.filter( (image,i) => i != activeImageIndex).map(image => <img className="student-dash-project-thumbnail" src={image.image_url}/>)
         return (
             edit 
             ?
-            <div className="student-dash-project">
+            <div className="student-dash-project editing">
                 Project Name: <input value={project_name} onChange={(e) => this.handleChange(e.target.value, 'project_name')}/>
                 Project Description: <input value={description} onChange={(e) => this.handleChange(e.target.value, 'description')}/>
                 Project Link: <input value={project_link} onChange={(e) => this.handleChange(e.target.value, 'project_link')}/>
@@ -53,7 +61,16 @@ export default class StudentProject extends Component {
                     images.map( i =>  {
                         return (
                             <div key={i.project_image_id}>
-                                Image Url: <input value={i.image_url} onChange={(e) => this.handleChange(e.target.value, 'images', i.project_image_id)}/>
+                                Image Url: <input value={i.image_url} onChange={(e) => this.handleImageChange(e.target.value, 'images', i.project_image_id)}/>
+                            </div>
+                        )
+                    })
+                }
+                {
+                    addedImages.map( i =>  {
+                        return (
+                            <div key={i.project_image_id}>
+                                Image Url: <input value={i.image_url} onChange={(e) => this.handleImageChange(e.target.value, 'addedImages', i.project_image_id)}/>
                             </div>
                         )
                     })
