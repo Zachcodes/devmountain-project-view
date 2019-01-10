48f998706ea8eb643608991f4f247993b8882286
zach <zaspringer@gmail.com>
2019-01-10 16:47:31 -0700

    Almost done for the day. This commit has all of state being updated. Now I need to handle saving, redirecting and setting newUser to false. I'm going to try to figure out why I can't maintain a session when reloading right now

--- src/Views/NewUser/NewUser.js
+++ src/Views/NewUser/NewUser.js
@@ -7,8 +7,8 @@ export default class NewUser extends Component {
         super()
         this.state = {
             student: {},
-            cohorts: {},
-            types: [],
+            cohorts: [],
+            cohortTypes: [],
             selectedType: 'none',
             selectedCohortId: 0
         }
@@ -16,11 +16,10 @@ export default class NewUser extends Component {
 
     componentDidMount() {
         axios.get('/api/students/info').then( res => {
-            let {student, cohorts} = res.data
-            let types = Object.keys(cohorts)
-            let o = this.setSelectedCohortAndType()
+            let {student, cohorts, cohortTypes} = res.data
+            let o = this.setSelectedCohortAndType(student.cohort, cohorts)
             let {selectedType, selectedCohortId} = o
-            this.setState({student, cohorts, types, selectedType, selectedCohortId})
+            this.setState({student, cohorts, cohortTypes, selectedType, selectedCohortId})
         })
     }
 
@@ -36,31 +35,37 @@ export default class NewUser extends Component {
     }
 
     handleChange(val, key) {
+        let obj = {}
+        obj[key] = val;
+        this.setState(obj)
+    }
+
+    handleStudentChange(val, key) {
         let obj = {...this.state.student}
         obj[key] = val;
         this.setState({student: obj})
     }
 
     render() {
-        let {about, cohort, email, first, github, image, last, linkedin, portfolio} = this.state.student
-        let {types, selectedType, selectedCohortId, cohorts} = this.state
+        let {about, email, first, github, image, last, linkedin, portfolio} = this.state.student
+        let {cohortTypes, selectedType, selectedCohortId, cohorts} = this.state
 
         return (
             <div>
                 <form onSubmit={this.handleSubmit}>
-                    <div>About: <input value={about} onChange={(e) => this.handleChange(e.target.value, 'about')}/></div>
+                    <div>About: <input value={about} onChange={(e) => this.handleStudentChange(e.target.value, 'about')}/></div>
                     <div>
                     Cohort Type: 
-                    <select name="types" value={selectedType}>
+                    <select name="cohortTypes" value={selectedType} onChange={(e) => this.handleChange(e.target.value, 'selectedType')}>
                         <option value={'none'}>None</option>
                         {
-                            types.map( t => {
+                            cohortTypes.map( t => {
                                 return <option value={t} key={t}>{t}</option>
                             })
                         }
                     </select>
                     Cohort:
-                    <select name="cohorts" value={selectedCohortId}>
+                    <select name="cohorts" value={selectedCohortId} onChange={(e) => this.handleChange(e.target.value, 'selectedCohortId')}>
                         <option value='none'>None</option>
                         {
                             cohorts.map( c => {
@@ -69,14 +74,13 @@ export default class NewUser extends Component {
                         }
                     </select>
                     </div>
-                    <div>Cohort: <input value={cohort} onChange={(e) => this.handleChange(e.target.value, 'cohort')}/></div>
-                    <div>Email: <input value={email} onChange={(e) => this.handleChange(e.target.value, 'email')}/></div>
-                    <div>First: <input value={first} onChange={(e) => this.handleChange(e.target.value, 'first')}/></div>
-                    <div>Github: <input value={github} onChange={(e) => this.handleChange(e.target.value, 'github')}/></div>
-                    <div>Image: <input value={image} onChange={(e) => this.handleChange(e.target.value, 'image')}/></div>
-                    <div>Last: <input value={last} onChange={(e) => this.handleChange(e.target.value, 'last')}/></div>
-                    <div>LinkedIn: <input value={linkedin} onChange={(e) => this.handleChange(e.target.value, 'linkedin')}/></div>
-                    <div>Portfolio: <input value={portfolio} onChange={(e) => this.handleChange(e.target.value, 'portfolio')}/></div>
+                    <div>Email: <input value={email} onChange={(e) => this.handleStudentChange(e.target.value, 'email')}/></div>
+                    <div>First: <input value={first} onChange={(e) => this.handleStudentChange(e.target.value, 'first')}/></div>
+                    <div>Github: <input value={github} onChange={(e) => this.handleStudentChange(e.target.value, 'github')}/></div>
+                    <div>Image: <input value={image} onChange={(e) => this.handleStudentChange(e.target.value, 'image')}/></div>
+                    <div>Last: <input value={last} onChange={(e) => this.handleStudentChange(e.target.value, 'last')}/></div>
+                    <div>LinkedIn: <input value={linkedin} onChange={(e) => this.handleStudentChange(e.target.value, 'linkedin')}/></div>
+                    <div>Portfolio: <input value={portfolio} onChange={(e) => this.handleStudentChange(e.target.value, 'portfolio')}/></div>
                     <button>Save Changes</button>
                 </form>
             </div>
